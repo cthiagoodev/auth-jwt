2613d36f2eda5ed4826699ae119188653325c9af
thiagosousadev <thiagosousadev@gmail.com>
2024-05-29 17:29:06 -0300

    config jwt

--- src/main/kotlin/br/com/thiagoodev/authjwt/infrastructure/config/SecurityConfig.kt
+++ src/main/kotlin/br/com/thiagoodev/authjwt/infrastructure/config/SecurityConfig.kt
@@ -3,19 +3,38 @@ package br.com.thiagoodev.authjwt.infrastructure.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val authenticationProvider: AuthenticationProvider,
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter,
+) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        return http.authorizeHttpRequests {
+        http.csrf { csrf -> csrf.disable() }
+
+        http.sessionManagement {
+            it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        }
+
+        http
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+
+        http.authorizeHttpRequests {
             it
                 .requestMatchers(HttpMethod.GET, "/login").permitAll()
                 .requestMatchers(HttpMethod.POST, "/register").permitAll()
-        }.build()
+                .anyRequest().authenticated()
+        }
+
+        return http.build()
     }
 }
