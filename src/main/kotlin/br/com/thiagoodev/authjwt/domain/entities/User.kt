4c6d609beef36b9710b71599e89da76a04da0435
thiagosousadev <thiagosousadev@gmail.com>
2024-05-31 11:19:31 -0300

    correcao migrations

--- src/main/kotlin/br/com/thiagoodev/authjwt/domain/entities/User.kt
+++ src/main/kotlin/br/com/thiagoodev/authjwt/domain/entities/User.kt
@@ -1,26 +1,25 @@
 package br.com.thiagoodev.authjwt.domain.entities
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.Table
+import jakarta.persistence.*
+import org.hibernate.annotations.UuidGenerator
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 import java.time.LocalDateTime
+import java.util.UUID
 
 @Entity
 @Table(name = "users")
 data class User(
     @Id
-    @field:GeneratedValue(strategy = GenerationType.AUTO)
-    val uuid: String = "",
+    @field:GeneratedValue(strategy = GenerationType.UUID)
+    @field:UuidGenerator
+    val uuid: UUID = UUID.randomUUID(),
     val name: String = "",
     @field:Column(unique = true)
     val email: String = "",
+    @field:Column(name = "password")
     val userPassword: String = "",
     @field:CreatedDate
     val created: LocalDateTime = LocalDateTime.now(),
